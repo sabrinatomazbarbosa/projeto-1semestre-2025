9e8746eea1b97e5555126de8e31d753d18bd9443
sabrinatomazbarbosa <sabrinabarbosatomaz@gmail.com>
2025-02-20 22:37:38 -0300

    atualizacao controller

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -1,13 +1,14 @@
 package com.fatec.projeto.projeto2025.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioController {
-    @GetMapping ("")
-    public String HelloWorld(){
-        return "hello";
+    @GetMapping ("(nome)")
+    public String HelloWorld( @PathVariable String nome){
+        return nome;
     }
     
 }
