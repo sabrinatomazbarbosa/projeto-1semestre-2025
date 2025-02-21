c58944d5d7bc0310ef71e2e66c49c31e78010261
sabrinatomazbarbosa <sabrinabarbosatomaz@gmail.com>
2025-02-20 22:44:54 -0300

    salvando ex1

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -1,14 +1,21 @@
 package com.fatec.projeto.projeto2025.controllers;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioController {
-    @GetMapping ("(nome)")
-    public String HelloWorld( @PathVariable String nome){
-        return nome;
+    @GetMapping ("")
+    public String HelloWorld(){
+        return "hello";
+    }
+
+    @GetMapping ("{nome}")
+    public String HelloWorld( @PathVariable Optional <String> nome){
+        return nome.isPresent() ? nome.get() : "dd";
     }
     
 }
