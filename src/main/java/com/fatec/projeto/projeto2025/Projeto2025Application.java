f36dd7bb0a226cb3c42b7ed17bffb8dcde199acb
sabrinatomazbarbosa <sabrinabarbosatomaz@gmail.com>
2025-02-27 21:15:02 -0300

    feat: retorna idade

--- src/main/java/com/fatec/projeto/projeto2025/Projeto2025Application.java
+++ src/main/java/com/fatec/projeto/projeto2025/Projeto2025Application.java
@@ -11,25 +11,9 @@ public class Projeto2025Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto2025Application.class, args);
 	}
+    
 
-	
-	@GetMapping("{idade}")
-   
-    public String classificaIdade(@PathVariable int idade){
- 
-        if (idade > 0 && idade < 12){
-            return "Criança";
-        }else if(idade >= 12 && idade <=18){
-            return "Adolescente";
-        }else if(idade >= 19 && idade <= 60){
-            return "Adulto";
-        }else if(idade > 60){
-            return "Idoso";
-        }else{
-            return "Idade inválida";
-        }
- 
-    }
+
 
 	
 
--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -1,6 +1,6 @@
 package com.fatec.projeto.projeto2025.controllers;
 
-import java.util.Optional;
+//import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioController {
-    @GetMapping ("")
+    /*@GetMapping ("")
     public String HelloWorld(){
         return "hello";
     }
@@ -16,6 +16,51 @@ public class ExercicioController {
     @GetMapping ("{nome}")
     public String HelloWorld( @PathVariable Optional <String> nome){
         return nome.isPresent() ? nome.get() : "dd";
+    }*/
+
+// @GetMapping("/get-idade/{idade}")
+//public String classificaIdade( integer idade){
+
+
+   @GetMapping("{idade}")
+
+    public String classificaIdade(@PathVariable int idade){
+        /*poderia ser 
+        try{
+        if(idade<0){
+        throw new NumberFormatException();
+        }
+        if (idade<12) {
+        return "Criança";
+        }else if (idade>=12 && idade<18){
+        return "Adolescente";
+        }else if (idade =18 && idade<60){
+        return "Adulto";
+        }else{
+        return "Idoso";
+        }
+
+
+        } 
+        catch(Exception e){
+        return "Idade inválida"
+        }
+
+
+
+        */
+        if (idade > 0 && idade < 12){
+            return "Criança";
+        }else if(idade >= 12 && idade <=18){
+            return "Adolescente";
+        }else if(idade >= 19 && idade <= 60){
+            return "Adulto";
+        }else if(idade > 60){
+            return "Idoso";
+        }else{
+            return "Idade inválida";
+        }
+ 
     }
     
 }
