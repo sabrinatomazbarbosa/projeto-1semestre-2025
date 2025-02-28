0df3bc6538c7b24deaa5f2e63b32a41ee18dda29
sabrinatomazbarbosa <sabrinabarbosatomaz@gmail.com>
2025-02-27 21:31:37 -0300

    ex5

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -8,14 +8,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioController {
-    @GetMapping ("")
+
+
+    @GetMapping("{idade}")
+   
+    public String classificaIdade(@PathVariable int idade){
+ 
+        if (idade > 0 && idade < 12){
+            return "Criança";
+        }else if(idade >= 12 && idade <=18){
+            return "Adolescente";
+        }else if(idade >= 19 && idade <= 60){
+            return "Adulto";
+        }else if(idade > 60){
+            return "Idoso";
+        }else{
+            return "Idade inválida";
+        }
+ 
+    }
+ 
+    /*@GetMapping ("")
     public String HelloWorld(){
         return "hello";
     }
+        */
 
-    @GetMapping ("{nome}")
+  /*   @GetMapping ("{nome}")
     public String HelloWorld( @PathVariable Optional <String> nome){
         return nome.isPresent() ? nome.get() : "dd";
     }
+        */
     
 }
