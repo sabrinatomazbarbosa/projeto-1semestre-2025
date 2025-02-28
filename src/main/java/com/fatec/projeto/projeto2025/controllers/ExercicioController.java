430936d32ab8ab59c09922f500a9aac66a8a3d5f
sabrinatomazbarbosa <sabrinabarbosatomaz@gmail.com>
2025-02-27 22:17:54 -0300

    exercicio 1 e 2 adicionados

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExercicioController {
 
 
-    @GetMapping("{idade}")
+    @GetMapping("/ex5/{idade}")
    
     public String classificaIdade(@PathVariable int idade){
  
@@ -27,6 +27,47 @@ public class ExercicioController {
         }
  
     }
+    @GetMapping("/ex1/{numero}")
+   
+    public String numeroparimpar(@PathVariable int numero){
+        if (numero>=0) {
+            if (numero % 2 == 0){
+                return "par";
+            }else{
+                return "ímpar";  
+            }   
+        } else{
+            return "Valor inválido";
+            
+        }
+    }
+    @GetMapping("/ex2/{peso}/{altura}")
+   
+    public String calculadora (@PathVariable double peso, @PathVariable double altura){
+        double imc;
+        imc = peso / (altura*altura);
+        if(imc >=0 ){
+            if(imc<=18.5){
+                return "abaixo do peso";
+            }else if(imc>18.5 &&  imc<=24.9){
+                return "peso normal";
+            }else if(imc>=25 && imc <=29.9){
+                return "sobrepeso";
+            }else if(imc>=30 && imc <= 34.9){
+                return "obesidade grau 1";
+            }else if(imc>=35.0 && imc <=39.0){
+            return "obesidade grau 2";
+            }else{
+                return "obesidade grau 3";
+            }
+        }else{
+            return "valor inválido";
+        }
+       
+    }
+
+
+
  
     /*@GetMapping ("")
     public String HelloWorld(){
@@ -41,3 +82,4 @@ public class ExercicioController {
         */
     
 }
+
